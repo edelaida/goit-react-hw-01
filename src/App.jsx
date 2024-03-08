0b020727acf4b54d8cc57dc674dca9b1c7567dde
Levina Tani <edelaida@gmail.com>
2024-03-08 12:17:16 +0200

    project-new

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Profiler } from "react";
+
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+function App() {
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <Profiler
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
